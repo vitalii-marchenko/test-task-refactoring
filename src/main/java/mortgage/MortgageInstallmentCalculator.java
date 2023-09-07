db15f543d27942952e56d1dc29f3a8e17687ee90
mcsymiv <s.mcsymiv@gmail.com>
2023-09-07 13:51:51 +0300

    update var names

---
+++ src/main/java/mortgage/MortgageInstallmentCalculator.java
@@ -0,0 +1,38 @@
+package mortgage;
+
+public class MortgageInstallmentCalculator {
+
+    public static int monthsInYear = 12;
+
+    /**
+     *
+     * @param principalAmount principal amount agreed to pay
+     * @param termInYears term of mortgage in years
+     * @param interestRate rate of interest, cost of borrowing the principal
+     * @return monthly payment amount
+     */
+    public static double calculateMonthlyPayment(
+            int principalAmount,
+            int termInYears,
+            double interestRate
+    ) {
+        if (principalAmount < 0 || termInYears <= 0 || interestRate < 0) {
+            throw new RuntimeException("Negative values are not allowed");
+        }
+
+        // convert interest rate into a decimal - eg. 6.5% = 0.065
+        interestRate /= 100.0;
+
+        // convert term in years to term in months
+        double termInMonths = termInYears * monthsInYear;
+
+        if (interestRate == 0) {
+            return principalAmount / termInMonths;
+        }
+
+        double interestRateInMonths = interestRate / monthsInYear;
+
+        // calculate the monthly payment
+        return (principalAmount * interestRateInMonths) / (1 - Math.pow(1 + interestRateInMonths, -termInMonths));
+    }
+}
--- src/test/java/MortgageInstallmentCalculatorTest.java
+++ src/test/java/MortgageInstallmentCalculatorTest.java
@@ -1,44 +1,47 @@
+import mortgage.MortgageInstallmentCalculator;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static mortgage.MortgageInstallmentCalculator.calculateMonthlyPayment;
 
 public class MortgageInstallmentCalculatorTest {
 
     @Test
-    public void shouldCalculateMonthlyPaymentWhenAmountsAreSmall() throws Exception {
-        double monthlyPaymentAmount = MortgageInstallmentCalculator.calculateMonthlyPayment(1000, 1, 12);
+    public void shouldCalculateMonthlyPaymentWhenAmountsAreSmall() {
+        double monthlyPaymentAmount = calculateMonthlyPayment(1_000, 1, 12);
         Assert.assertEquals(88.84d, monthlyPaymentAmount, 0.01d);
     }
 
     @Test
-    public void shouldCalculateMonthlyPaymentWhenAmountIsLarge() throws Exception {
-        double monthlyPaymentAmount = MortgageInstallmentCalculator.calculateMonthlyPayment(10000000, 1, 12);
+    public void shouldCalculateMonthlyPaymentWhenAmountIsLarge() {
+        double monthlyPaymentAmount = calculateMonthlyPayment(10_000_000, 1, 12);
         Assert.assertEquals(888487.88d, monthlyPaymentAmount, 0.01d);
     }
 
     @Test
-    public void shouldCalculateMonthlyPaymentWhenPrincipalIsZero() throws Exception {
-        double monthlyPaymentAmount = MortgageInstallmentCalculator.calculateMonthlyPayment(0, 1, 12);
+    public void shouldCalculateMonthlyPaymentWhenPrincipalIsZero() {
+        double monthlyPaymentAmount = calculateMonthlyPayment(0, 1, 12);
         Assert.assertEquals(0, monthlyPaymentAmount, 0.01d);
     }
 
     @Test
-    public void shouldCalculateMonthlyPaymentWhenInterestRateIsZero() throws Exception {
-        double monthlyPaymentAmount = MortgageInstallmentCalculator.calculateMonthlyPayment(1000, 1, 0);
+    public void shouldCalculateMonthlyPaymentWhenInterestRateIsZero() {
+        double monthlyPaymentAmount = calculateMonthlyPayment(1000, 1, 0);
         Assert.assertEquals(83.33, monthlyPaymentAmount, 0.01d);
     }
 
     @Test(expected = RuntimeException.class)
-    public void shouldThrowInvalidInputExceptionOnNegativeTenure() throws Exception {
-        MortgageInstallmentCalculator.calculateMonthlyPayment(20, -10, 14.5);
+    public void shouldThrowInvalidInputExceptionOnNegativeTenure() {
+        calculateMonthlyPayment(20, -10, 14.5);
     }
 
     @Test(expected = RuntimeException.class)
-    public void shouldThrowInvalidInputExceptionOnNegativeInterestRate() throws Exception {
-        MortgageInstallmentCalculator.calculateMonthlyPayment(20, 1, -12);
+    public void shouldThrowInvalidInputExceptionOnNegativeInterestRate() {
+        calculateMonthlyPayment(20, 1, -12);
     }
 
     @Test(expected = RuntimeException.class)
-    public void shouldThrowInvalidInputExceptionOnNegativePrincipalAmount() throws Exception {
-        MortgageInstallmentCalculator.calculateMonthlyPayment(-20, 10, 14.5);
+    public void shouldThrowInvalidInputExceptionOnNegativePrincipalAmount() {
+        calculateMonthlyPayment(-20, 10, 14.5);
     }
 }
